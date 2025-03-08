fe6f2872c8e5919cd2d0d5dfd8f64d39f233a7a2
vercettiprada <164518236+vercettipra@users.noreply.github.com>
2025-03-08 15:57:30 +0800

    added "id"

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,10 +1,11 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Models\User;
-use App\Traits\ApiResponser; // Import the ApiResponser trait
+use App\Traits\ApiResponser;
 use DB;
 
 class UserController extends Controller
@@ -18,43 +19,39 @@ class UserController extends Controller
         $this->request = $request;
     }
 
-    /**
-     * Retrieve all users from the database.
-     */
     public function getUsers()
     {
         $users = DB::connection('mysql')->select("SELECT * FROM tbl_user");
-        
-        // return response()->json($users, 200);
         return $this->successResponse($users);
     }
 
-    /**
-     * Retrieve all users using Eloquent ORM.
-     */
     public function index()
     {
         $users = User::all();
-
-        // Alternative return formats:
-        // return response()->json($users, 200);
-        // return response()->json(['data' => $users, 'site' => 1], 200);
-        // return response()->json(['data' => $users], 200);
-
         return $this->successResponse($users);
     }
-    public function add (Request $request){
-        $rules =[
 
-        'username' => 'required|max:20',
-        'password' => 'required|max:20',
-        'gender' => 'required|max:20',
+    public function add(Request $request)
+    {
+        $rules = [
+            'username' => 'required|max:20',
+            'password' => 'required|max:20',
+            'gender' => 'required|max:20',
         ];
-        
-        $this ->validate($request,$rules);
 
+        $this->validate($request, $rules);
         $user = User::create($request->all());
-
         return $this->successResponse($user, Response::HTTP_CREATED);
+    }
+
+    public function show($id)
+    {
+        $user = User::where('id', $id)->first();
+
+        if ($user) {
+            return $this->successResponse($user);
+        } else {
+            return $this->errorResponse('User Id Does Not Exist', Response::HTTP_NOT_FOUND);
+        }
     }
 }
